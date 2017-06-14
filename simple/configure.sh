0a6c17ab74a8513f131ef979ba1dbbf2b9b2524f
左凌轩 <hzzuolingxuan@corp.netease.com>
2017-06-14 14:12:41 +0800

    add simple config for server

---
+++ simple/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -d ~/.vim/bundle/Vundle.vim ]; then
+        echo 'vunle installed'
+else
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ ! `which ctags` ]; then
+        echo 'please install ctags'
+fi
+
+cp ./vimrc ~/.vimrc
+cp ./vimrc.bundle ~/.vimrc.bundle
+
+vim -u "~/.vimrc" "+BundleInstall" "+qall"
